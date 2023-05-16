44c6e2f84398a056c53bd0f6283d4811daa02545
Maksymilian Czudziak <maks.czudziak@gmail.com>
2023-05-16 23:00:33 +0200

    Order class implemented

---
+++ include/order.hpp
@@ -0,0 +1,36 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include "unit.hpp"
+
+struct move_t
+{
+    int x;
+    int y;
+};
+struct attack_t
+{
+    Unit *prey;
+};
+struct build_t
+{
+    char letter;
+};
+
+union parameters_t
+{
+    struct move_t move;
+    struct attack_t attack;
+    struct build_t build;
+};
+
+class Order
+{
+public:
+    int unit_id;
+    char action;
+    parameters_t parameters;
+    std::string denote(void);
+};
+
+#endif
